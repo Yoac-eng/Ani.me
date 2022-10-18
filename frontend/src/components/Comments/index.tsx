3d8dbac71e5dc0841b4aa363202540c9eb93ab4a
Yoac <cayocutrim1991@gmail.com>
2022-10-18 11:58:15 -0300

    fixing commentary image

--- frontend/src/components/Comments/index.tsx
+++ frontend/src/components/Comments/index.tsx
@@ -3,9 +3,9 @@ import * as S from './styles'
 
 export function Comments() {
     const comments: Array<any> = [
-    {imgSrc: "testando.jpg", name: "Daniel Cazé", commentary: "  transar de ladinho é mt poggers a piroca entrando e saindo kekw ai tu da uma segurada no peitinho e dale na metelancia mid gap only bota a mão no pescocinho dela e tome micro dif o foda eh so quando o dick escapa da pussy ai é omegalul total."},
-    {imgSrc: "testando.jpg", name: "Daniel Cazé", commentary: "  transar de ladinho é mt poggers a piroca entrando e saindo kekw ai tu da uma segurada no peitinho e dale na metelancia mid gap only bota a mão no pescocinho dela e tome micro dif o foda eh so quando o dick escapa da pussy ai é omegalul total."},
-    {imgSrc: "testando.jpg", name: "Daniel Cazé", commentary: "  transar de ladinho é mt poggers a piroca entrando e saindo kekw ai tu da uma segurada no peitinho e dale na metelancia mid gap only bota a mão no pescocinho dela e tome micro dif o foda eh so quando o dick escapa da pussy ai é omegalul total."}
+    {imgSrc: "/testando.jpg", name: "Daniel Cazé", commentary: "  transar de ladinho é mt poggers a piroca entrando e saindo kekw ai tu da uma segurada no peitinho e dale na metelancia mid gap only bota a mão no pescocinho dela e tome micro dif o foda eh so quando o dick escapa da pussy ai é omegalul total."},
+    {imgSrc: "/testando.jpg", name: "Daniel Cazé", commentary: "  transar de ladinho é mt poggers a piroca entrando e saindo kekw ai tu da uma segurada no peitinho e dale na metelancia mid gap only bota a mão no pescocinho dela e tome micro dif o foda eh so quando o dick escapa da pussy ai é omegalul total."},
+    {imgSrc: "/testando.jpg", name: "Daniel Cazé", commentary: "  transar de ladinho é mt poggers a piroca entrando e saindo kekw ai tu da uma segurada no peitinho e dale na metelancia mid gap only bota a mão no pescocinho dela e tome micro dif o foda eh so quando o dick escapa da pussy ai é omegalul total."}
     ]
     return (
         <S.CommentsWrapper>
